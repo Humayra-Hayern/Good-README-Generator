d6392d0211b409b1343e2c184b4583248f688851
Humayra Hayern <hayern786@hotmail.com>
2022-07-07 16:45:44 +0100

    fixed eclipse license

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const questions = [
       'BSD 3-Clause "New" or "Revised" License',
       "Boost Software License 1.0",
       "Creative Commons Zero v1.0 Universal",
-      "Eclipse Public License 2.0",
+      "Eclipse Public License 1.0",
       "GNU Affero General Public License v3.0",
       "GNU General Public License v2.0",
       "Mozilla Public License 2.0",
