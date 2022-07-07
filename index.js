0898f09b195ba06b9d4b9f4cc58898b2e91c6fdb
Humayra Hayern <hayern786@hotmail.com>
2022-07-07 16:16:57 +0100

    installed inquirer and added questions/prompts

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of the project:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter the description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter the installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the usage information:",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Test instructions:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license:",
+    choices: [
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      'BSD 2-Clause "Simplified" License',
+      'BSD 3-Clause "New" or "Revised" License',
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
